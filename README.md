1422945550f6f6a4a6c4b7ccc687eb9eaff4399a
ksyasuda <ksyasuda@umich.edu>
2020-12-03 05:42:08 -0800

    Add readme and fix bugs with classroom questions
    Fix last day not being added to map and fix last set of exams not being
    formatted correctly.

---
+++ README.md
@@ -0,0 +1,52 @@
+# Sudacode Persona 5 Royal Guide Script
+During the first Coronavirus quarantine, I bought the original Persona 5 while
+it was on sale in the Playstation Store on a whim.  I absolutely loved it and
+the game has become one of my favorites.  Recently, during a two week quarantine
+after returning home from college, I bought Persona 5 Royal, which is the
+remastered version of Persona 5 with additional content.
+
+During my playthrough of Persona 5, not Royal, it took me a while to get up to
+speed and figure out what I should be doing each day and how I should go about
+leveling up my confidants and social stats.
+
+I found myself constantly refrencing online guides for where certain personas
+could be found, which days will net you extra charm at the bathhouse, and
+answers to class questions.  As a result, I decided to create this script to
+help provide all the information that I need all in one place.
+
+## Content
+* [Download](#download)
+* [Usage](#usage)
+
+## Download <a name='download'></a>
+Run the following commands in your terminal to download the repository and
+`cd` into the downloaded directory:
+
+	$ git clone https://github.com/ksyasuda/Sudacode-Persona5RoyalGuide.git && \
+	cd Sudacode-Persona5RoyalGuide
+
+## Usage <a name='usage'></a>
+The program is a Python (Python3) script that parses text files in the repository
+containing information about topics in Persona 5 Royal.
+
+To run the script, use one of the following commands from within the directory:
+
+	$ ./persona5-royal-guide.py [args]
+	or
+	$ python persona5-royal-guide.py [args]
+
+### Copying the Script to the `bin` folder
+By copying, or creating a symlink in the `bin` folder, you can run the command
+without declaring the path.  For example I created a symlink by running, from
+within the Persona5RoyalGuide directory:
+
+	sudo ln -sr persona5-royal-guide.py /bin/persona5-royal-guide
+
+Then the script can be executing by using the command `$ persona5-royal-guide`
+
+### Command Line Arguments
+|	Argument	|		  Description			    |
+|---------------|-----------------------------------|
+| -h, --help	|	Bring up the help menu		    |
+| -v, --verbose	|	Toggle verbose output		    |
+| -c, --class	|	Get class/exam question answers |
--- p5rClassroomQuestions.py
+++ p5rClassroomQuestions.py
@@ -4,38 +4,47 @@ import sys
 import os
 
 def getInput(isVerbose):
+    """Gets input from the user about the current month and day"""
     monthMap = {'JANUARY': 1, 'FEBRUARY': 2, 'MARCH': 3, 'APRIL': 4, 'MAY': 5, 'JUNE': 6, 'JULY': 7, 'AUGUST': 8, 'SEPTEMBER': 9, 'OCTOBER': 10, 'NOVEMBER': 11, 'DECEMBER': 12, 'JAN': 1, 'FEB': 2, 'MAR': 3, 'APR': 4, 'MAY': 5, 'JUN': 6, 'JUL': 7, 'AUG': 8, 'SEP': 9, 'OCT': 10, 'NOV': 11, 'DEC': 12}
     if isVerbose:
         print('Getting User Data...')
-    month = input('Which Month? (String or Integer): ')
+    month = input('Which Month (String or Integer)? ')
     # if input is a string then convert it into its numerical equivalent
     if not month.isnumeric():
-        month = monthMap[month.upper()]
-    day = input('What Day? (Integer): ')
+        tempMonth = month.upper()
+        if tempMonth in monthMap:
+            month = monthMap[tempMonth]
+        else:
+            print(month, 'is not a valid month')
+            sys.exit(1)
+    day = input('What Day (Integer)? ')
     datestr = str(month) + "/" + str(day)
     if isVerbose:
         print('User input received... generating and returning date string', datestr)
     return datestr
 
 def printData(days, day, isVerbose):
+    """Print answers to questions on day [day]"""
     if isVerbose:
         print('Searching dictionary for date string', day)
     if day in days:
         if isVerbose:
             print(day, 'found in dictionary... printing information about the day')
         for key in days[day]:
-            # [Q/A]: [Question/Answer] 
+            ## [Q/A]: [Question/Answer] 
             print(key, days[day][key])
     else:
         print(day, 'is not a day where questions are asked in class')
 
 def readQuestions(days, isVerbose):
-    """Read the questions from the file at~/Documents/P5RClassroomAnswers.txt, then parses it into the days dict"""
-    # get path of current file, following sym-links
+    """Read the questions from the file at $CWD/P5RClassroomAnswers.txt, then parses it into the days dict"""
+    ## get path of current file, following sym-links
     pathToScript = os.path.realpath(__file__)
     # take off the filename and last '/'
     filepath = pathToScript[0:pathToScript.rfind('/')]
     filepath += '/P5RClassroomAnswers.txt'
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+              'August', 'November', 'December']
     with open(filepath) as f:
         lines = f.readlines()
         # first line will be date 4/12
@@ -45,20 +54,18 @@ def readQuestions(days, isVerbose):
         for i in range(len(lines)):
             # first date already set skip
             # don't need empty lines either
-            if i == 0 or lines[i] == '\n':
+            if i == 0 or lines[i][:lines[i].find(' ')] in months:
                 continue
             parts = lines[i].split(' ')
             if len(parts) == 1:
                 # date string
-                # days[parts[0].rstrip()] = day
                 days[date.rstrip()] = day
                 if isVerbose:
                     print('DATE: ', parts[0], 'Q/A', day)
                 day = {}
                 date = parts[0] 
-            elif len(parts) == 2 and parts[0] == 'EXAMS':
+            elif len(parts) > 2 and parts[0] == 'EXAMS':
                 # exams date string
-                # days[parts[1].rstrip()] = day
                 days[date.rstrip()] = day
                 date = parts[1]
                 if isVerbose:
@@ -67,7 +74,7 @@ def readQuestions(days, isVerbose):
             else:
                 # append to the day dictionary the entire question/answer
                 day[parts[0]] = ' '.join(parts[1:]).rstrip()
+        ## One last day of questions
+        days[date.rstrip()] = day
         if isVerbose:
             print('Finished loading dictionary... ready for user input')
-        # chosenDate = getInput(isVerbose)
-        # printData(days, chosenDate, isVerbose)
