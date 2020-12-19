266e6ba7e4f6430504fd87b2701763e743c8fcfc
ksyasuda <ksyasuda@umich.edu>
2020-12-19 01:49:01 -0800

    Update main to be pycodestyle compliant

--- persona5-royal-guide.py
+++ persona5-royal-guide.py
@@ -3,10 +3,12 @@
 from P5RGuide.Utility.utility import get_opts
 from P5RGuide.Utility.utility import handle_options
 
+
 def main():
     """The literal main"""
     options, remainder, argc, argv = get_opts()
     handle_options(options, remainder, argc, argv)
+
 
 if __name__ == '__main__':
     main()
