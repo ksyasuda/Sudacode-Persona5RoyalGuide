305c6182f761079592d103402d04524eeba2f714
ksyasuda <ksyasuda@umich.edu>
2020-12-20 12:58:22 -0800

    Add makoto and kawakami gift guides functionality

--- P5RGuide/ConfidantGuides/GiftGuide.py
+++ P5RGuide/ConfidantGuides/GiftGuide.py
@@ -2,18 +2,23 @@ import re
 from P5RGuide.ConfidantGuides.DialogueGuide import get_path_to_file
 from P5RGuide.colors.colors import bcolors
 
-AVAILABLE_CONFIDANTS = ['Ann Takamaki']
+AVAILABLE_CONFIDANTS = ['Ann Takamaki', 'Sadayo Kawakami', 'Makoto Nijima']
 
 def printAvailableConfidants():
     """Prints the list of available confidants for the gifts"""
     for c in AVAILABLE_CONFIDANTS:
-        print(c, end='\t')
+        print(c, end='    ')
+    print()
 
 def getPathToGift(filepath, confidant, isVerbose):
     """Returns the filepath to the best gifts for the given confidant"""
     filepath += '/Gifts'
     if confidant == 'Ann Takamaki':
         filepath += '/AnnGifts.txt'
+    elif confidant == 'Sadayo Kawakami':
+        filepath += '/KawakamiGifts.txt'
+    elif confidant == 'Makoto Nijima':
+        filepath += '/MakotoGifts.txt'
     return filepath
 
 def getScoreIdx(parts, isVerbose):
@@ -41,27 +46,19 @@ def putColors(parts, begin, end, colors):
 
 def checkP5R(parts):
     """Checks for the P5R string in the message"""
-    line = ' '.join(parts[:])
-    pattern = re.compile(r'\([0-9A-Za-z\+\s]+P5R\)')
-    match = pattern.search(line)
-    if match is not None:
-        # return (match.start(), match.end())
-    else:
-        return -1
-    # idx = 0
-    # begin = end = 999
-    # if '(P5R' in parts or 'P5R)' in parts:
-    #     for part in parts:
-    #         if part[0] == '(':
-    #             begin = idx
-    #         if part[-1] == ')':
-    #             end = idx
-    #         idx += 1
-    #     if begin == 999 or end == 999:
-    #         return -1
-    #     print(begin, end)
-    #     return (begin, end)
-    # return -1
+    idx = 0
+    begin = end = 999
+    if '(P5R' in parts or 'P5R)' in parts:
+        for part in parts:
+            if part[0] == '(':
+                begin = idx
+            if part[-1] == ')':
+                end = idx
+            idx += 1
+        if begin == 999 or end == 999:
+            return -1
+        return (begin, end)
+    return -1
 
 def checkSpecialChars(line, s_char, color, isVerbose):
     """Returns the colored special characters (^*) in the string"""
